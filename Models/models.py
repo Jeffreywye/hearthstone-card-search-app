c7d34996ba369b0e52990f1e6d56a3d053e07a07
Jeffreywye <jeffreywye7@gmail.com>
2023-03-26 07:52:55 -0400

    finished one relationship

--- Models/models.py
+++ Models/models.py
@@ -2,8 +2,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# card_class = db.Table('CardClass',
-#                       db.Column())
+card_class = db.Table('CardClass',
+    db.Column('card_id', db.String(50), db.ForeignKey('Cards.id'), primary_key = True),
+    db.Column('class_id', db.Integer, db.ForeignKey('Classes.id'), primary_key = True)
+)
 
 class card(db.Model):
     __tablename__ = "Cards"
@@ -15,3 +17,19 @@ class card(db.Model):
     mana = db.Column(db.Integer)
     health = db.Column(db.Integer)
     attack = db.Column(db.Integer)
+    classes = db.relationship("Classes",
+        secondary = card_class,
+        back_populates = "cards",
+        lazy = "dynamic"
+    )
+
+# when a class is delete, delete all assoc cards in Cards Table
+class class_(db.Model):
+    __tablename__ = "Classes"
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    name = db.Column(db.String(30))
+    cards = db.relationship("Cards",
+        secondary = card_class,
+        back_populates = 'classes',
+        cascade="all, delete",
+        lazy = 'dynamic')
--- app.py
+++ app.py
@@ -28,5 +28,6 @@ def index():
     return "hello world"
 
 if __name__ == '__main__':
+    test()
     # app.run()
     pass
